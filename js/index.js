8f129155c9ab8b5b54b7f13abd8ed7e3732f36d7
JuanCruu <juancruznavarro592@gmail.com>
2021-05-24 19:29:05 -0300

    trabajando en el posicionamiento de imagenes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    // setTimeout(function() {
-    //     alert("Hello");
-    // }, 3000);
+    setTimeout(function() {
+        let loading = document.getElementById("loading");
+        let contenido = document.getElementById("contenido");
+        loading.style.display = "none"
+        contenido.style.display = "inline"
+        contenido.style.animation = "fadeIn 1s ease-in"
+
+    }, 500);
+
+
+    let chica = document.getElementById("chica");
+
+    window.addEventListener('scroll', () => {
+        let value = window.scrollY + 1100;
+        console.log(chica.style.right);
+        console.log(value);
+
+        chica.style.right = value + "px";
+
+        chica.style.width = (value - 150) * 0.3 + "px";
+        chico.style.left = value + "px";
+
+
+        tierra.style.top = window.scrollY * 0.2 + "px";
+        tanque.style.top = -window.scrollY + "px";
+        chatarra.style.right = value + "px";
+        chatarra.style.transform = "rotate(" + window.scrollY * 0.1 + "deg)";
+        marciano.style.transform = "rotate(" + window.scrollY * 0.1 + "deg)"
+
+
+    })
 })
